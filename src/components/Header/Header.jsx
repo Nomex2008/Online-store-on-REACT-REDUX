c83173de360e982cdf57dbc556d43efc0b6b13e2
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-09-28 23:55:50 +0300

    Commit 3(header1)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
+import styles from '../../styles/Header.module.css'
+
+import {ROUTERS} from'../../utils/routes.js'
+
+import { Link } from 'react-router-dom';
+
+import logo from '../../images/logo.svg'
+import avatar from '../../images/avatar.jpg'
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className={styles.header}>
+      <div className="logo">
+        <Link to={ROUTERS.HOME}>
+          <img src={logo} alt=" " />
+        </Link>
+
+        <div className="info">
+          <div className="user">
+            <div className="avatar" style={{backgroundImage: `url(${avatar})`}}></div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
