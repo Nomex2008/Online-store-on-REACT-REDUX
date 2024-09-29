39eca844b94171ab45baee605b8a8e0bc86dcd2d
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-09-29 19:03:39 +0300

    Commit 4(header2)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,15 +12,46 @@ import avatar from '../../images/avatar.jpg'
 const Header = () => {
   return (
     <div className={styles.header}>
-      <div className="logo">
+      <div className={styles.logo}>
         <Link to={ROUTERS.HOME}>
-          <img src={logo} alt=" " />
+          <img src={logo} alt="Stuff" />
         </Link>
+      </div>
 
-        <div className="info">
-          <div className="user">
-            <div className="avatar" style={{backgroundImage: `url(${avatar})`}}></div>
+      <div className={styles.info}>
+        <div className={styles.user}>
+          <div className={styles.avatar} style={{backgroundImage: `url(${avatar})`}}/>
+          <div className={styles.username}>Guest</div>
+        </div>
+
+        <form className={styles.form}>
+          <div className={styles.icon}>
+            <svg className='icon'>
+              <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#search`}></use>
+            </svg>
           </div>
+          <div className={styles.input}>
+            <input type="search" name='search' 
+            placeholder='Search for anything...'
+              autoComplete="off" onChange={() => {}} value=""/>
+          </div>
+
+          {false && <div className={styles.box}></div>}
+        </form>
+
+        <div className={styles.account}>
+          <Link to={ROUTERS.HOME} className={styles.favorites}>
+            <svg className={styles['icon-fav']}>
+              <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#heart`}></use>
+            </svg>
+          </Link>
+
+          <Link to={ROUTERS.CART} className={styles.cart}>
+            <svg className={styles['icon-cart']}>
+              <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#bag`}></use>
+            </svg>
+            <span className={styles.count}>2</span>
+          </Link>
         </div>
       </div>
     </div>
