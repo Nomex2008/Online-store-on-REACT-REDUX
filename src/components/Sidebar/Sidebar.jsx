398d8da331d964619aa93b7bc315a9ddfda0e55f
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-10-01 00:41:27 +0300

    Commit 8(sidebar2)
    Add map and categories for sidebar.

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 import { NavLink } from 'react-router-dom'
 
 import styles from '../../styles/Sidebar.module.css'
 
 const Sidebar = () => {
+  const { list } = useSelector(({categories}) => categories)
+  //console.log(list)
+
   return (
     <section className={styles.sidebar}>
       <div className={styles.title}>CATEGORIES</div>
       <nav>
         <ul className={styles.menu}>
-          <li>
-            <NavLink to={`/categories/${1}`}>
-              Link
-            </NavLink>
-          </li>
+        {list.map(({ id, name }) => (
+            <li key={id}>
+              <NavLink
+                className={({ isActive }) =>
+                  `${styles.link} ${isActive ? styles.active : ""}`
+                }
+                to={`/categories/${id}`}
+              >
+                {name}
+              </NavLink>
+            </li>
+          ))}
         </ul>
       </nav>
 
