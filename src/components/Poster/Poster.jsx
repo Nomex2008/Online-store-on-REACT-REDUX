d35d68d36db7a5ff3f4f4b517e9857fa79b2aff8
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-10-02 23:18:17 +0300

    Commit 9(Poster1)
    Add Poster.jsx

---
+++ src/components/Poster/Poster.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from '../../styles/Home.module.css'
+
+import BG from '../../images/computer.png'
+
+const Poster = () => (
+    <section className={styles.home}></section>
+)
+
+export default Poster;
