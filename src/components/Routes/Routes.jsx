519b7a123944249ecaddca73ea6693a28cd252db
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-10-14 23:49:56 +0300

    Commit 19(Profile1)
    Add start Profile component.

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { ROUTES } from '../../utils/routes.js';
 import { Home } from '../Home/Home.jsx';
 import SingleProduct from '../Products/SingleProduct.jsx';
+import Profile from '../Profile/Profile.jsx';
 
 const AppRoutes = () => {
   return (
@@ -10,6 +11,7 @@ const AppRoutes = () => {
       {/* Визначаємо шлях для роута */}
       <Route path="/" element={<Home />} />
       <Route path={ROUTES.PRODUCT} element={<SingleProduct/>}/>
+      <Route path={ROUTES.PROFILE} element={<Profile/>}/>
     </Routes>
   );
 };
