25e48563ee8bb50bde46fc5a133cc1b180bfccdb
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-10-17 23:42:16 +0300

    Commit 21(SingleCategory1)
    Add component SingleCategory

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ROUTES } from '../../utils/routes.js';
+import SingleCategory from '../Categories/SingleCategory.jsx';
 import { Home } from '../Home/Home.jsx';
 import SingleProduct from '../Products/SingleProduct.jsx';
 import Profile from '../Profile/Profile.jsx';
@@ -12,8 +13,9 @@ const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path={ROUTES.PRODUCT} element={<SingleProduct/>}/>
       <Route path={ROUTES.PROFILE} element={<Profile/>}/>
+      <Route path={ROUTES.CATEGORY} element={<SingleCategory/>}/>
     </Routes>
   );
 };
 
-export default AppRoutes
+export default AppRoutes;
